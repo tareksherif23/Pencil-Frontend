8855437ee14026e8252e1f7fc40b2b3e5be0f2a1
Tarek Marzouk <tarek.marzouk@ibm.com>
2020-12-06 19:14:22 +0200

    Added LaTeX functionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit} from '@angular/core';
 import MediumEditor from 'medium-editor';
+import { evaluate } from 'mathjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 
@@ -17,7 +18,6 @@ export class HomeComponent implements OnInit {
   @ViewChild('container')
   container!: ElementRef;
   
-
 constructor(public auth: AngularFireAuth,public fs: AngularFirestore) { }
 
   ngOnInit() {
@@ -26,22 +26,22 @@ constructor(public auth: AngularFireAuth,public fs: AngularFirestore) { }
       else this.isUser = false})
   }
 
-   ngAfterViewInit() {
-    const element = this.container.nativeElement;
-    const editor = new MediumEditor(element,{ toolbar: {
-      allowMultiParagraphSelection: true,
-      buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
-      diffLeft: 0,
-      diffTop: -10,
-      firstButtonClass: 'medium-editor-button-first',
-      lastButtonClass: 'medium-editor-button-last',
-      standardizeSelectionStart: false,
-      static: false,
-      align: 'center',
-      sticky: false,
-      updateOnEmptySelection: false
-  }});
-  
+ ngAfterViewInit() {
+  const element = this.container.nativeElement;
+  const editor = new MediumEditor(element,{ toolbar: {
+    allowMultiParagraphSelection: true,
+    buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'],
+    diffLeft: 0,
+    diffTop: -10,
+    firstButtonClass: 'medium-editor-button-first',
+    lastButtonClass: 'medium-editor-button-last',
+    standardizeSelectionStart: false,
+    static: false,
+    align: 'center',
+    sticky: false,
+    updateOnEmptySelection: false
+}});
+
 
   this.auth.user.subscribe(user=>{
     if (user) this.isUser = true;
@@ -57,13 +57,75 @@ constructor(public auth: AngularFireAuth,public fs: AngularFirestore) { }
     })
   }
 }) 
-  
+
   editor.subscribe('editableInput', (event, editable)=>{
+    this.latex(editor)
     this.fs.doc('users/'+this.userId).set({
     note: editor.getContent()
     })
 });
    
 }
+
+
+private latex(editor: any): void {
+ 
+  console.log("Hello from latex")
+  const content = editor.getContent(0);
+
+  //  some filtering
+  if (content.lastIndexOf('$') <= content.indexOf('$') + 3) return;
+
+  //  get a pure text version of the content
+  const notag = new RegExp(/<[a-zA-Z\/][^>]*>/, 'igm');
+  const noTagContent = content.replace(notag, '');
+
+  let cursorPos = editor.exportSelection();
+
+  //  find all strings between $ signs 
+  let matches = content.match(/\$(.*?)\$/igm)
+  
+  //  evaluate arithmetic expressions
+  let solutions: string[] = [];
+
+  for (let i in matches) {
+    let match = matches[i];
+    match = match.replace(/\$/igm, '');
+    match = match.replace(notag, '');
+
+    try {
+      match = (evaluate(match));
+    } catch (e) {
+      match = null;
+    }
+    solutions[i] = match;
+  };
+
+  //  if there is nothing to replace then return
+  if (solutions.filter(x => !!x).length <= 0) return;
+
+  //  replace expressions in the text with results
+  //  formatting tags will be gone!
+  solutions.forEach((solution, index) => {
+    if (!solution) return;
+    if(!matches) return;
+    const matchNoTag = matches[index].replace(notag, '');
+    editor.importSelection({
+      start: noTagContent.lastIndexOf(matchNoTag),
+      end: noTagContent.lastIndexOf(matchNoTag) + matchNoTag.length
+    },true);
+    editor.pasteHTML(solution);
+    if (noTagContent.substr(cursorPos.end - 1, matches[index].length) !== matches[index]) {
+      cursorPos.start -= matchNoTag.length - String(solution).length;
+    } else {
+      cursorPos.start--;
+    }
+    cursorPos.end = cursorPos.start;
+  });
+
+  //  move cursor back to where it was
+  editor.importSelection(cursorPos, false);
+} 
+
 
 }
